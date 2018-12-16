b5a750dcc85c42d69e37b27cfc32ecffefe30b77
RoboticHamster357 <marta.kutkowska357@gmail.com>
2018-12-16 16:19:52 +0100

    Książki zamiast pytań

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from polls.models import Author, Genre, Book, BookInstance
 
-# Register your models here.
-
-
-admin.site.register(Question)
-
-admin.site.register(Choice)
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
